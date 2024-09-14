68294e3026d8a014d2d77d6ad3109070679ed483
FrostyLock <158774773+FrostyLock@users.noreply.github.com>
2024-09-14 18:21:55 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -6,8 +6,8 @@ def add(x, y, z):
 def subtract(x, y, z):
     return x - y + z
 
-def multiply(x, y):
-    return x * y
+def multiply(x, y, z):
+    return x * y * z
 
 def divide(x, y):
     if y == 0:
